05ff39d97b2145fddee54c0dd631338b42275c25
Vincent MONTECOT <vmonteco@e2r12p13.42.fr>
2017-09-25 20:42:39 +0200

    run subparser added

--- expert_system.py
+++ expert_system.py
@@ -1,4 +1,46 @@
 #!/usr/bin/env python3
 
-if __name__ == '__main__':
+import argparse
+
+
+################################################################################
+#                              Command parser                                  #
+################################################################################
+
+def make_parser():
+    """
+    make_parser() just generates the expert_system.py arguments parser.
+    """
+
+    parser = argparse.ArgumentParser(description='Inference engine.')
+    subparsers = parser.add_subparsers(dest="subcommand")
+    subparsers.required = True
+    solver_subparser = subparsers.add_parser('run')
+    solver_subparser.add_argument(
+        '-v', '--verbose', help='enable verbose mode.', action='store_true'
+    )
+    solver_subparser.add_argument(
+        '-d', '--debug', help='enable debug mode.', action='store_true'
+    )
+    solver_subparser.add_argument(
+        'filename', type=str,
+        help='filename containing the instructions to process.'
+    )
+    return parser
+
+def run(filename, verbose, debug):
+    """
+    Empty function so far.
+    """
     pass
+
+
+################################################################################
+#                                Entrypoint                                    #
+################################################################################
+
+if __name__ == '__main__':
+    parser = make_parser()
+    args = parser.parse_args()
+    if args.subcommand == 'run':
+        run(args.filename, verbose=args.verbose, debug=args.debug)
