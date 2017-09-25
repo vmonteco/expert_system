069a37a3bd1b65521764b8f0ad180ed5104caaf6
Vincent MONTECOT <vmonteco@e2r12p13.42.fr>
2017-09-25 21:26:20 +0200

    added clean_line()

--- src/parsing.py
+++ src/parsing.py
@@ -4,16 +4,32 @@
 #                                File parser                                   #
 ################################################################################
 
+space_regex = re.compile(' |\t|\n')
+
+def clean_line(l):
+    """
+    clean_line() removes possible comments and space characters (using
+    space_regex from global scope).
+    """
+    
+    return space_regex.sub( # replace occurences by first argument
+        '',
+        l.split('#')[0])
+
+
 def parse(filename):
     """
     parse() parses the file content, interprets rules and initial states, and
     returns queries.
     """
+
+    state = 0 # state permits to keep track of the parsing steps.
     
     with open(filename) as f:
         for line in f:
-            pass
+            line = clean_line(line) # removing comments and spaces
     return queries
+
 
 if __name__ == '__main__':
     pass
